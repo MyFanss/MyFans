d353d3d7686f958f766f9b4e26107ed542562326
mimah97 <e77377366@gmail.com>
2026-02-20 00:47:46 +0100

    cors

--- myfans-backend/src/main.ts
+++ myfans-backend/src/main.ts
@@ -3,11 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { applyAppConfig } from './app.config';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.useGlobalFilters(new HttpExceptionFilter());
   applyAppConfig(app);
 
   const corsOrigin = configService.get<string>('CORS_ORIGIN', 'http://localhost:3000');
