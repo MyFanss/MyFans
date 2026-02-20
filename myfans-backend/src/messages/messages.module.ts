061255c283ae45108a0e0996ae894afb83aeb7bf
abdegenius <johnnieabijah2000@gmail.com>
2026-02-20 19:37:02 +0100

    feat: added the message and room module

---
+++ myfans-backend/src/messages/messages.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Conversation } from './entities/conversation.entity';
+import { Message } from './entities/message.entity';
+import { MessagesService } from './messages.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Conversation, Message])],
+  providers: [MessagesService],
+  exports: [MessagesService],
+})
+export class MessagesModule {}
