d8fe64f5ccdca8a21f005508a9bff1f1b7a1840e
yahia008 <yahyatijjani99@jmail.com>
2026-02-21 13:14:45 +0100

    ci/cd issue

--- backend/src/app-test.module.ts
+++ backend/src/app-test.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NotificationModule } from './notification/notification.module';
+
 
 @Module({
-  imports: [NotificationModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
