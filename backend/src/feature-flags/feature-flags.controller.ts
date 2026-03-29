806aad96bc4b89f19e510ee76a81ee344c22a8a0
Vibeofkd <yusufhayatu830@gmail.com>
2026-03-29 01:34:01 +0100

    feat Add role-based access control to APIs.

--- backend/src/feature-flags/feature-flags.controller.ts
+++ backend/src/feature-flags/feature-flags.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { FeatureFlagsService } from './feature-flags.service';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
 @Controller({ path: 'feature-flags', version: '1' })
 export class FeatureFlagsController {
   constructor(private readonly featureFlagsService: FeatureFlagsService) {}
